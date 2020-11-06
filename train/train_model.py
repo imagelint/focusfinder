15be10571d060ba1f3015aa1f6dd495d1e2f3637
Kilian Freitag <kilian.freitag@diefreitags.de>
2020-11-06 14:36:22 +0100

    feat: model and data now take around 7000 images

--- train/train_model.py
+++ train/train_model.py
@@ -5,35 +5,27 @@ import os
 
 print("Starting Focusfinder train script")
 
-# define functions that return a focus point for a given file_name
-def label_func(path_name):
-    x = re.search(r'\d\w+.jpg', str(path_name))
-    return x.group()
-
-def get_focus_point(x):
-    file_name = label_func(x)
-    dfb = next(iter(df[df['name']==file_name].index), ('no match '+file_name))
+def get_focus_point(path_name):
+    dfb = next(iter(df[df['name']==path_name.name].index), ('no match for '+path_name.name))
     return tensor([float(df['x_p'][dfb]), float(df['y_p'][dfb])])
 
 # set paths for images and label.csv
 file_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = "/".join(file_path.split("/")[0:-1])
 path = Path(dir_path)
-labels_path = path/'download/labels/norm_labels_4000.csv'
-images_path = path/'download/norm_images'
-
+labels_path = path/'download/labels/train_labels.csv'
+images_path = path/'download/images/norm_images'
 # load csv
-df = pd.read_csv(labels_path, names=['name','x_p','y_p'], header=0)
-
+df = pd.read_csv(labels_path)
 # define fastai datablock
 imgs = DataBlock(
     blocks=(ImageBlock, PointBlock),
     get_items=get_image_files,
     get_y=get_focus_point,
     splitter=RandomSplitter(valid_pct=0.2, seed=42),
-    batch_tfms=[*aug_transforms(size=(256, 256)), 
+    batch_tfms=[*aug_transforms(size=(244, 244)),
                 Normalize.from_stats(*imagenet_stats)],
-    item_tfms=Resize(256),
+    item_tfms=Resize(244),
 )
 
 # create dataloader
@@ -45,7 +37,7 @@ learn = cnn_learner(dls, resnet18, y_range=(-1,1))
 
 # train model with epochs and learn_rate (good learn rate can be found with fastai functin learn.lr_find())
 epochs = 3
-train_rate = 2e-5
+train_rate = 4e-5 # lr_min=0.012022644281387329, lr_steep=3.630780702224001e-05
 
 print("Starting training with train_rate {} for {} epochs\n".format(train_rate, epochs))
 learn.fine_tune(epochs, train_rate)
